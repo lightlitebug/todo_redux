9c2330ffd0077cddcfada27a63e2ab5e4c3029a9
lightlitebug <light.lite.bug@gmail.com>
2022-12-01 23:35:28 +0900

    FilterTodo widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'filter_todo.dart';
 import 'new_todo.dart';
 import 'search_todo.dart';
 import 'todo_header.dart';
@@ -20,6 +21,8 @@ class TodosPage extends StatelessWidget {
                 NewTodo(),
                 SizedBox(height: 20.0),
                 SearchTodo(),
+                SizedBox(height: 20.0),
+                FilterTodo(),
               ],
             ),
           ),
