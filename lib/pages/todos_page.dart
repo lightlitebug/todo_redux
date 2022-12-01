6d6f8885d8d24a03b6c8b18d0425168aa33c7d64
lightlitebug <light.lite.bug@gmail.com>
2022-12-01 22:40:28 +0900

    TodoHeader widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'todo_header.dart';
+
 class TodosPage extends StatelessWidget {
   const TodosPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TODOS'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: const [
+                TodoHeader(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
