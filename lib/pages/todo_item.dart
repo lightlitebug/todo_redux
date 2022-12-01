1a1383d740128ca4c14ff3e52d0530c4f121c554
lightlitebug <light.lite.bug@gmail.com>
2022-12-02 01:05:19 +0900

    TodoItem widget 2

--- lib/pages/todo_item.dart
+++ lib/pages/todo_item.dart
@@ -17,6 +17,14 @@ class TodoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return ConfirmEditDialog(todo: todo);
+          },
+        );
+      },
       leading: StoreConnector<AppState, _ToggleViewModel>(
         distinct: true,
         converter: (Store<AppState> store) => _ToggleViewModel.fromStore(store),
@@ -48,6 +56,92 @@ class _ToggleViewModel extends Equatable {
       toggleTodo: (String id) => store.dispatch(
         ToggleTodoAction(id: id),
       ),
+    );
+  }
+}
+
+class ConfirmEditDialog extends StatefulWidget {
+  const ConfirmEditDialog({
+    Key? key,
+    required this.todo,
+  }) : super(key: key);
+  final Todo todo;
+
+  @override
+  State<ConfirmEditDialog> createState() => _ConfirmEditDialogState();
+}
+
+class _ConfirmEditDialogState extends State<ConfirmEditDialog> {
+  late final TextEditingController textController;
+  bool error = false;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController(text: widget.todo.todoDesc);
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Edit Todo'),
+      content: TextField(
+        controller: textController,
+        autofocus: true,
+        decoration: InputDecoration(
+          errorText: error ? "Value cannot be empty" : null,
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('CANCEL'),
+        ),
+        StoreConnector<AppState, _EditViewModel>(
+          distinct: true,
+          converter: (Store<AppState> store) => _EditViewModel.fromStore(store),
+          builder: (BuildContext context, _EditViewModel vm) {
+            return TextButton(
+              onPressed: () {
+                error = textController.text.isEmpty ? true : false;
+                if (error) {
+                  setState(() {});
+                } else {
+                  vm.editTodo(widget.todo.id, textController.text);
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('EDIT'),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class _EditViewModel extends Equatable {
+  final void Function(String id, String todoDesc) editTodo;
+  const _EditViewModel({
+    required this.editTodo,
+  });
+
+  @override
+  List<Object> get props => [];
+
+  static fromStore(Store<AppState> store) {
+    return _EditViewModel(
+      editTodo: (String id, String todoDesc) {
+        store.dispatch(
+          EditTodoAction(id: id, todoDesc: todoDesc),
+        );
+      },
     );
   }
 }
